9aca147a08d9095a31b55d195d81f7f337868709
Unknown <nirmalmandhata@gmail.com>
2018-07-17 15:40:47 +0530

    Initial commit to demo project

---
+++ src/main/java/MyWorld.java
@@ -0,0 +1,12 @@
+
+class MyWorld{
+	public static void main(String arg[]) {
+		String message = welcomeMessage();
+		System.out.println("\n\n"+message);
+		System.out.println("success");
+	}
+	
+	public static String welcomeMessage() {
+		return "Wellcome to my world";
+	}
+}
